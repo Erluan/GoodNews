c377dc96310c48a7f3d644dcd49bc3d287a9c8bd
Erluan Felix Batista <erluan@gmail.com>
2022-02-12 16:42:28 -0300

    :hammer: Creating ArticleTableViewCell

---
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by Erluan Felix Batista on 12/02/22.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
