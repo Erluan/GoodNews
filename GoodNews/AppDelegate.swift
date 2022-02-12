a1c3f7098e1da2df82abcbc98230d50c691de47e
Erluan Felix Batista <erluan@gmail.com>
2022-02-12 16:43:19 -0300

    :hammer: removing comments

--- GoodNews/AppDelegate.swift
+++ GoodNews/AppDelegate.swift
@@ -13,15 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-//        UINavigationBar.appearance().barTintColor = UIColor(displayP3Red: 47/255, green: 54/255, blue: 64/255, alpha: 1.0)
-//        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
-//        let barAppearance = UINavigationBarAppearance()
-//        barAppearance.backgroundColor = .red
-//        
-//        navigationBar.standardAppearance = barAppearance
-//        navigationBar.scrollEdgeAppearance = barAppearance
-        
         return true
     }
 
